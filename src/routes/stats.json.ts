317b951505d98a358963e7ff3d296b85fc7291ff
hans <hkmelaa@hotmail.com>
2022-01-26 01:16:09 +0100

    endpoint for stats

---
+++ src/routes/stats.json.ts
@@ -0,0 +1,26 @@
+export async function get(requestEvent) {
+	const db = requestEvent.locals.db;
+	const sessions = await db.getSessions();
+
+	const today = new Date();
+	const sessionsToday = sessions.filter((session) => {
+		const { epoch } = session;
+		return new Date(epoch).getDate() === today.getDate();
+	});
+
+	const data = sessions.reduce(
+		(acc, session) => {
+			const { browser, os } = session;
+			if (!acc.browser[browser]) acc.browser[browser] = 0;
+			if (!acc.os[os]) acc.os[os] = 0;
+
+			acc.browser[browser]++;
+			acc.os[os]++;
+
+			return acc;
+		},
+		{ os: {}, browser: {}, sessions: sessions.length, today: sessionsToday.length }
+	);
+
+	return { body: { data } };
+}
