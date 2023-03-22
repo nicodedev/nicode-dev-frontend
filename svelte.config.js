4b8b46584bd6601eba09fa16682a8724a1e85048
Nicolai Gjellestad <nicolai@gjellestad.net>
2023-03-22 13:46:55 +0100

    Update svelte kit

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
